146c150885dfed595c461ce6686c330a12bb285d
ferminmilito <133276527+ferminmilito@users.noreply.github.com>
2023-12-16 21:02:29 -0300

    Add files via upload

---
+++ dragon.js
@@ -0,0 +1,45 @@
+class dragon {
+constructor(posX,posY,ancho,alto){
+    this.posX= posX;
+    this.posY=posY;
+    this.alto=alto;
+    this.ancho=ancho;
+    this.colordragon= color(255,0,200);
+    this.vida= 3;
+  }
+ 
+  dibujar(){
+    rectMode(CENTER);
+    noFill();
+    noStroke();
+    rect(this.posX,this.posY,this.ancho,this.alto);
+    image(personaje,this.posX-90,this.posY-70);
+  }
+  
+  
+  teclapresionada(keyCode){
+   if(keyCode == LEFT_ARROW){
+     this.moverIzquierda();
+   }else if( keyCode == RIGHT_ARROW){
+     this.moverDerecha();
+  }
+  }
+  
+  
+  moverDerecha (){
+    this.posX+=35;
+   }
+  
+  moverIzquierda(){
+  this.posX -= 35;
+  }
+  
+  vidas(){
+    
+  }
+  
+  
+  
+  
+  
+}
